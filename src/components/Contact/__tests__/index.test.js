7c54677fe734bb6b2de9747029e13557ec50be6c
polizoto <polizoto@hotmail.com>
2022-03-29 07:01:24 -0400

    add test for Modal component

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Contact from '..';
+// __tests__/Contact.test.js
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Contact from '..'
 
-const form = { name: "John Doe", email: "johndoe@gmail.com", message: "I am your biggest fan!" };
+afterEach(cleanup)
 
-afterEach(cleanup);
+describe('Contact component', () => {
+  it('renders', () => {
+    render(<Contact />);
+  });
 
-describe('Contact is rendering', () => {
-
-    it('renders', () => {
-      render(<Contact />);
-    });
-  })
-
-  it('matches snapshot', () => {
+  it('renders', () => {
     const { asFragment } = render(<Contact />)
     expect(asFragment()).toMatchSnapshot()
   })
+})
 
-  it('renders', () => {
-    const { getByTestId } = render(<Contact />)
-    expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
-  })  
 
-  it('renders', () => {
-    const { getByTestId } = render(<Contact />)
-    expect(getByTestId('buttontag')).toHaveTextContent('Submit')
-  })  
+it('renders', () => {
+  const { getByTestId } = render(<Contact />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
+})
+
+it('renders', () => {
+  const { getByTestId } = render(<Contact />)
+  expect(getByTestId('button')).toHaveTextContent('Submit')
+})
